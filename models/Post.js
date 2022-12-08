c650f970c33d8d6a4f5e61e2878b4d06083afd24
Riwa Sherpa <riwasherpa@Riwas-MBP.lan>
2022-12-08 12:15:12 -0800

    added handlebars

---
+++ models/Post.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// Creating Post model
+class Post extends Model {}
+
+// create fields/columns for Post model
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: ture,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    descripton: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
+  }
+);
+
+module.exports = Post;
